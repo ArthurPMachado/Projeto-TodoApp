cc27c319075732e3d79d3949b62ce7d60aca9eff
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-02 16:00:59 -0300

    Feat: create Todo and About pages

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,16 @@
 import React from 'react';
+import Todo from './pages/Todo';
+import About from './pages/About';
+
+// Import bootstrap and font-awesome
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Todo />
+      <About />
     </div>
   );
 }
