53cadfc0b858a86c17e8a397d714880a299ba3f6
Arthur Pereira Machado <machadoparthur1@gmail.com>
2020-12-04 12:20:23 -0300

    Feat: render task list and delete buttons

--- web/src/components/TodoList.jsx
+++ web/src/components/TodoList.jsx
@@ -1,10 +1,41 @@
+/* eslint-disable react/style-prop-object */
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
+import IconButton from './IconButton';
 
-function TodoList() {
+function TodoList(props) {
+  const { list } = props || [];
+  const { handleRemove } = props;
+
+  function renderRows() {
+    return (
+      list.map((todo) => (
+        <tr key={todo._id}>
+          <td>{todo.description}</td>
+          <td>
+            <IconButton
+              style="danger"
+              icon="trash-o"
+              onClick={() => handleRemove(todo)}
+            />
+          </td>
+        </tr>
+      ))
+    );
+  }
+
   return (
-    <div>
-      <h1>List</h1>
-    </div>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
   );
 }
 
--- web/src/pages/Todo.jsx
+++ web/src/pages/Todo.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+/* eslint-disable no-underscore-dangle */
+import React, { useEffect, useState } from 'react';
 import api from '../services/api';
 
 import PageHeader from '../components/PageHeader';
@@ -9,13 +10,33 @@ function Todo() {
   const [description, setDescription] = useState('');
   const [list, setList] = useState([]);
 
+  useEffect(() => {
+    api.get('?sort=-createdAt').then((response) => {
+      setDescription('');
+      setList(response.data);
+    });
+  }, []);
+
+  function handleRefresh() {
+    api.get('?sort=-createdAt').then((response) => {
+      setDescription('');
+      setList(response.data);
+    });
+  }
+
   function handleChange(event) {
     setDescription(event.target.value);
   }
 
   function handleAddTask() {
     api.post('', { description }).then((response) => {
-      console.log('Funcionou');
+      handleRefresh();
+    });
+  }
+
+  function handleRemove(todo) {
+    api.delete(`${todo._id}`).then((response) => {
+      handleRefresh();
     });
   }
 
@@ -27,7 +48,7 @@ function Todo() {
         description={description}
         handleChange={handleChange}
       />
-      <TodoList />
+      <TodoList list={list} handleRemove={handleRemove} />
     </div>
   );
 }
